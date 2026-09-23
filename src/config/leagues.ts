4b896ea7adb73aa384c38cdb290a6628bad1220b
erowery <erowery@gmail.com>
2026-09-23 08:57:12 +0200

    Add files via upload

--- src/config/leagues.ts
+++ src/config/leagues.ts
@@ -6,8 +6,8 @@ export interface LeagueConfig {
 }
 
 export const LEAGUES_CONFIG: LeagueConfig[] = [
-  { id: 'lsdd1', name: 'I Liga LSDD', shortName: 'I Liga', nakkaId: 't_vqad_6773' },
-  { id: 'lsdd2', name: 'II Liga LSDD', shortName: 'II Liga', nakkaId: 't_RmkP_8273' },
-  { id: 'lsdd3', name: 'III Liga LSDD', shortName: 'III Liga', nakkaId: 't_hlYd_1847' },
-  { id: 'lsdd4', name: 'IV Liga LSDD', shortName: 'IV Liga', nakkaId: 't_lmUL_2298' },
+  { id: 'lsdd1', name: 'I Liga LSDD', shortName: 'I Liga', nakkaId: 't_iKfq_6965' },
+  { id: 'lsdd2', name: 'II Liga LSDD', shortName: 'II Liga', nakkaId: 't_pjgk_4087' },
+  { id: 'lsdd3', name: 'III Liga LSDD', shortName: 'III Liga', nakkaId: 't_Hlu8_7788' },
+  { id: 'lsdd4', name: 'IV Liga LSDD', shortName: 'IV Liga', nakkaId: 't_YOvw_3453' },
 ];
